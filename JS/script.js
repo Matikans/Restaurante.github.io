2b59a652dc9245ab63ea16a278aa8fc2ec452462
Matias Yllanes <116925063+Matikans@users.noreply.github.com>
2025-06-02 18:16:39 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+"use strict";
+var menuIcon = document.getElementById("menu-icon");
+var nav = document.getElementById("nav");
+var closeBtn = document.getElementById("close-btn");
+
+
+
+menuIcon.addEventListener("click", function(){
+    nav.classList.add("open");
+});
+closeBtn.addEventListener("click", function(){
+    nav.classList.remove("open");
+});
